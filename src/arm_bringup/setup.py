a9929aaeb524149ab70d3dbdc0a1229b2d8b504d
Ibrahim Ali <IbrahimMohammadAli@hotmail.com>
2026-02-07 18:52:26 -0500

    Fixed launch and run issues with setup.py files in packages

--- src/arm_bringup/setup.py
+++ src/arm_bringup/setup.py
@@ -1,15 +1,17 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'arm_bringup'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -19,7 +21,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
